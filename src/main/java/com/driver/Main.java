e52c84b3352f8c11d6054b5da0c25b636395da47
Rohit Gupta <rohitgupta0810@gmail.com>
2023-12-13 13:24:41 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,26 @@ package com.driver;
 
 public class Main {
 
+ public class Product{
+  public int product(int x, int y) {
+            return x*y;
+  }
+
+public int product(int x, int y, int z) {
+      return x*y*z;
 }
+public double product(double x, double y) {}
+            return x*y;
+
+ }
+
+  public static void main(String[]ags)[
+        Product p = new Product();
+        int x = 4;
+        int y = 5;
+        p.product(x,y);
+  ]
+
+
+}
+
